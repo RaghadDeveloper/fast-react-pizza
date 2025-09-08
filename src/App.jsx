a2d190d700e30bc3e5852f148f1551578eb3fcf5
RaghadDeveloper <raghadmh620@gmail.com>
2025-09-08 14:32:03 +0300

    Fetching orders

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./ui/Home";
 import Cart from "./features/cart/Cart";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -17,7 +17,7 @@ function App() {
         { path: "/menu", element: <Menu />, loader: menuLoader },
         { path: "/cart", element: <Cart /> },
         { path: "/order/new", element: <CreateOrder /> },
-        { path: "order/:orderId", element: <Order /> },
+        { path: "order/:orderId", element: <Order />, loader: orderLoader },
       ],
     },
   ]);
