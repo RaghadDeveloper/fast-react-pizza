34eae32ff13ac4e63392da66567091aa87cbc993
RaghadDeveloper <raghadmh620@gmail.com>
2025-09-10 08:52:20 +0300

    Update UI components and styles with Tailwind CSS

---
+++ src/ui/Button.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to, type }) {
+  const base =
+    "text-sm bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " py-3 px-4 md:px-6 md:py:4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "uppercase text-sm font-semibold text-stone-800 inline-block tracking-wide rounded-full transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:text-stone-800 focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed border-2 border-stone-300 py-2.5 px-4 md:px-6 md:py:3.5",
+  };
+
+  if (to)
+    return (
+      <Link className={styles[type]} to={to}>
+        {children}
+      </Link>
+    );
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
+  to: PropTypes.any,
+  type: PropTypes.string,
+};
+
+export default Button;
